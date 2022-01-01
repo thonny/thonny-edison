32c93bb15d3bbb77eee7ec46ff7ff2b6a7509833
Aivar Annamaa <aivar.annamaa@gmail.com>
2022-01-01 21:39:00 +0200

    Move testing code

--- compile_examples.py
+++ compile_examples.py
@@ -2,12 +2,13 @@ import os.path
 import subprocess
 import json
 
-edpy_dir = os.path.join(os.path.dirname(__file__), "EdPy")
+edpy_dir = os.path.join(os.path.dirname(__file__), "thonnycontrib", "backend",
+                        "edison_backend", "EdPy")
 examples_dir = os.path.join(os.path.dirname(__file__), "examples")
 for name in os.listdir(examples_dir):
     if name.endswith(".py"):
         try:
-            out = subprocess.check_output(["python3.8", os.path.join(edpy_dir, "EdPy.py"),
+            out = subprocess.check_output(["python3.9", os.path.join(edpy_dir, "EdPy.py"),
                                            "-a", os.path.join(examples_dir, name + ".asm"),
                                            "en_lang.json", os.path.join(examples_dir, name)],
                                     cwd=edpy_dir)
